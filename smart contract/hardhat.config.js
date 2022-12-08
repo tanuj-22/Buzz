af2e431a7cfed5add3c5ffb1bb4c9a171be2eb2c
tanuj-22 <56783833+tanuj-22@users.noreply.github.com>
2022-12-08 21:11:56 +0530

    initial build completed

--- smart contract/hardhat.config.js
+++ smart contract/hardhat.config.js
@@ -1,12 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.2",
   networks: {
     goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/`+ process.env.ALCHEMY_API_KEY,
+      url: `https://eth-goerli.g.alchemy.com/v2/` + process.env.ALCHEMY_API_KEY,
       accounts: [process.env.GOERLI_PRIVATE_KEY],
     },
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
+  },
+  solidity: "0.8.2",
 };
